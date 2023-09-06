d78d538933b773e64f6ed7bde4e939c1c1b49fbe
yynyzy <1601530253@qq.com>
2023-09-06 23:09:17 +0800

    backend: init yzyn backend

---
+++ src/main/java/com/yzyn/YzynBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yzyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzynBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YzynBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzyn/YzynBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzynBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
